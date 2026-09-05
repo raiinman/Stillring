42c100984b923381aadeb654317a5df66eb08abc
ShiroMaō Studios <42227255+raiinman@users.noreply.github.com>
2026-09-05 03:17:38 -0700

    Gate 1: use current UE editor build defaults

--- game/Source/StillringEditor.Target.cs
+++ game/Source/StillringEditor.Target.cs
@@ -6,7 +6,7 @@ public class StillringEditorTarget : TargetRules
     public StillringEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.AddRange(new[] { "Stillring", "StillringDev" });
     }
