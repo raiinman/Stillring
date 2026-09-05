2d33f5f66363c86c916f460cfc1fd303a4df9bbd
ShiroMaō Studios <42227255+raiinman@users.noreply.github.com>
2026-09-05 03:16:56 -0700

    Gate 1: register sample System IDE workbench

--- game/Source/StillringDev/StillringDev.cpp
+++ game/Source/StillringDev/StillringDev.cpp
@@ -1,10 +1,24 @@
 #include "Modules/ModuleManager.h"
+#include "SystemIDE/StillringSystemIDE.h"
+#include "SystemIDE/StillringHarnessWorkbench.h"
 
 class FStillringDevModule final : public IModuleInterface
 {
 public:
-    virtual void StartupModule() override {}
-    virtual void ShutdownModule() override {}
+    virtual void StartupModule() override
+    {
+        HarnessWorkbench = MakeShared<FStillringHarnessWorkbench>();
+        FStillringSystemIDERegistry::Get().RegisterWorkbench(HarnessWorkbench.ToSharedRef());
+    }
+
+    virtual void ShutdownModule() override
+    {
+        FStillringSystemIDERegistry::Get().UnregisterWorkbench(TEXT("Gate1Harness"));
+        HarnessWorkbench.Reset();
+    }
+
+private:
+    TSharedPtr<FStillringHarnessWorkbench> HarnessWorkbench;
 };
 
 IMPLEMENT_MODULE(FStillringDevModule, StillringDev);
